7d4295cf915ed561198ff0172df85508c8abad5b
jaysukla <am7343249@gmail.com>
2023-02-23 20:41:09 +0530

    fs

---
+++ server.js
@@ -0,0 +1,111 @@
+const express = require("express");
+const mongoose = require('mongoose')
+var cors = require('cors')
+const connection = mongoose.connect('mongodb+srv://JayShukla:jayshukla@cluster0.9zippbx.mongodb.net/MYCAL?retryWrites=true&w=majority')
+const app= express();
+app.use(cors())
+app.use(express.json())
+ let Dbmodel ;
+let email;
+// Creating new user collection  step 2 
+function CDB(nameofcollection){
+email=nameofcollection;
+const DBCmodel = mongoose.model(nameofcollection,mongoose.Schema({
+    title:String,
+    discription:String,
+    startdate:String,
+    enddate:String,
+    starttime:String,
+    endtime:String,
+    email:String
+}))
+
+Dbmodel=DBCmodel
+}
+// Creating new user collection step 2
+
+
+
+
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send("Server is Active ")
+})
+
+// Creating new user collection step 1
+app.post("/regis",(req,res)=>{
+
+    let data= req.body;
+
+CDB(data.email)
+
+res.send({"msg":"usercollection is created "})
+
+});
+// Creating new user collection step 1
+
+// new event creating 
+app.post("/newevent", async(req,res)=>{
+let data = req.body;
+
+let d= await Dbmodel.aggregate([{$match:{"startdate":data.startdate, "starttime":data.starttime}}])
+console.log(data.startdate)
+
+let m= await Dbmodel.aggregate([{$match:{"startdate":data.startdate}}])
+
+let c= 0;
+for(let i=0;i<m.length;i++){
+    if (m[i].endtime>=data.starttime){
+        c++
+    }
+}
+// console.log(d)
+if(d.length>0){
+    res.send({"msg":"TIME sLOT IS NOT AVAILABLE"})
+} 
+
+
+
+  
+else if(c>0){
+    res.send({"msg":"TIME sLOT IS NOT AVAILABLE"})
+}else{
+  
+        Dbmodel.insertMany([data])
+    res.send({"msg":"new event added "})
+    
+    
+}
+
+
+
+
+})
+
+// new event creating 
+
+app.get("/allevents",async(req,res)=>{
+
+    let data = await Dbmodel.find()
+
+res.send({"msg":"le bhai data","Data":data})
+
+})
+
+
+
+app.listen(8000,()=>{
+
+try {
+    connection;
+    console.log("Connection to db successful")
+} catch (error) {
+    
+}
+
+    console.log("Listning to port 8000")
+})
+
